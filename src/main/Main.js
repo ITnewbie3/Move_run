5cfdbdd91df4529abb581835f28416dafe87d43e
suhan <sd98221@naver.com>
2022-07-27 17:50:17 +0900

    안됨

--- src/main/Main.js
+++ src/main/Main.js
@@ -7,20 +7,24 @@ import './style.css'
 const Main = () => {
     let back = document.querySelectorAll('.inbox')
     let backLeft = useRef(0);
-    let current1 = useRef(0);
+    let divbox = useRef();
     let timer; // 마우스 hover시 정지
-
+    useEffect(() => {
         back.forEach((back,index)=>{
             back.style.left = `${index*845}px`
         })
+    },[])
+
+        
         useEffect(() => {
     function moveDiv(Left){
-            document.querySelector('#midbox').style.left = `${-Left * 845}px`;
+            divbox.current.style.left = `${Left * 845}px`;
         }
     function startIt(){
         timer = setInterval(function(){
            backLeft.current = backLeft.current >= back.length-1 ? 0 : backLeft.current+1;
             moveDiv(backLeft.current)
+            console.log(backLeft.current)
         },4000)
         function stopIt() {
             clearInterval(timer);
@@ -31,11 +35,11 @@ const Main = () => {
 },[backLeft])
 const nextimg =() => {
     backLeft.current = backLeft.current >= back.length-1 ? 0 : backLeft.current+1;
-    document.querySelector('#midbox').style.left = `${-backLeft.current * 845}px`;
+    divbox.current.style.left = `${-backLeft.current * 845}px`;
 }
     return (
         <div id='topbox'>
-            <div id='midbox'>
+            <div id='midbox' ref={divbox}>
             <div className='inbox' 
             style={{backgroundImage: 'url(/img/movie.gif)'}}>
     
